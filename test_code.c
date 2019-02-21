ae8f4e0412c7a6c7a027f142fcf32aecf613ad1e
gsabotball <47871061+gsabotball@users.noreply.github.com>
2019-02-21 14:13:04 -0800

    Add test code.
    First code is test_code to test Github.

---
+++ test_code.c
@@ -0,0 +1,17 @@
+#include <kipr/botball.h>
+
+/*
+*
+	This is test code just for messing with Github and
+	downloading programs from the Wallaby.
+*
+*/
+
+int main()
+{
+	int x;
+	for(x=0; x<10; x++){
+		printf("This code has compiled and run successfully %d time(s)!\n", x+1);
+	}
+	return 0;
+}
